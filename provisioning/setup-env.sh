7776ed559124e9329fc33946ec9364a2ebb411cb
Roman Verchikov <roman.verchikov@gmail.com>
2015-01-21 15:56:39 -0800

    Added DB setup

--- provisioning/setup-env.sh
+++ provisioning/setup-env.sh
@@ -22,6 +22,8 @@ done
 
 [[ -z $VAGRANT_USER && $(id -u $VAGRANT_USER) ]] && die "There must be an existing user name supplied. Check your calling script"
 
+sudo apt-get install git vim python-virtualenv python-pip postgresql postgresql-contrib libpq-dev python-dev libevent-dev libffi-dev -y
+
 if [[ -d $OSTF_SOURCES && -f $OSTF_SOURCES/requirements.txt ]]; then
     echo "Using existing $OSTF_SOURCES dir for OSTF sources"
 else
@@ -36,16 +38,25 @@ else
     popd
 fi
 
-sudo apt-get install git vim python-virtualenv python-pip postgresql postgresql-contrib libpq-dev python-dev libevent-dev -y
-
 if [[ -f $OSTF_SOURCES/requirements.txt ]]; then
     pushd $OSTF_SOURCES
-    run virtualenv ubuntu-venv
-    run PATH=$OSTF_SOURCES/ubuntu-venv/bin:$PATH env pip install -r requirements.txt
-    run PATH=$OSTF_SOURCES/ubuntu-venv/bin:$PATH env python setup.py develop
+    VENV_NAME=ubuntu-venv
+    run virtualenv $VENV_NAME
+    run PATH=$OSTF_SOURCES/$VENV_NAME/bin:$PATH env pip install -r requirements.txt
+    run PATH=$OSTF_SOURCES/$VENV_NAME/bin:$PATH env python setup.py develop
+
+    SQLALCHEMY_URL=$(grep '^sqlalchemy\.url.*=.*' $(find $OSTF_SOURCES -name 'alembic.ini'))
+    DB_USER=$(echo $SQLALCHEMY_URL | sed -r 's?.*//(\w+):(\w+)@(\w+)/.*?\1?')
+    DB_PASS=$(echo $SQLALCHEMY_URL | sed -r 's?.*//(\w+):(\w+)@(\w+)/.*?\2?')
+    OSTF_DB_NAME=$(echo $SQLALCHEMY_URL | sed -r 's?.*//(\w+):(\w+)@(\w+)/(\w+).*?\4?')
+    sudo -u postgres psql -c "CREATE DATABASE $OSTF_DB_NAME;"
+    sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
+    echo "local $OSTF_DB_NAME $DB_USER trust" >> $(find /etc -name 'pg_hba.conf')
+    /etc/init.d/postgresql restart
+    run PATH=$OSTF_SOURCES/$VENV_NAME/bin:$PATH env alembic -c $(find $OSTF_SOURCES -name 'alembic.ini') upgrade head
     popd
 else
-    die "Missing requirements.txt file, looks like $OSTF_SOURCES points to the wrong directory"
+    die "Missing requirements.txt file, make sure $OSTF_SOURCES contains fuel-ostf sources"
 fi
 
 echo "OSTF dev environment successfully set!"
